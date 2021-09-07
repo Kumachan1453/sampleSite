4c550fac037c67f2d19a966c5d056b1abe0dd4ec
阪本周平 <github000only@gmail.com>
2021-09-07 19:59:49 +0900

    現状

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-document.getElementById("navbtn").onclick = function(){
-    // console.log(`ボタンを押した`);
+document.getElementById("header").onclick = function(){
+    console.log(`ボタンを押した`);
     document.querySelector('html').classList.toggle('open');
 }
-document.getElementsByClassName("web").onclick = function(){
-    document.querySelectorAll('html').classList.remove('open');
-}
 
+// document.getElementsByClassName("headerList").onclick = function(){
+    // console.log(`自動で戻る`);
+    // document.querySelector('headerList').classList.toggle('open');
+// }
 
 // Document の getElementById() メソッドは、 id プロパティが指定された文字列に一致する要素を表す Element オブジェクトを返します。要素の ID は指定されていれば固有であることが求められているため、特定の要素にすばやくアクセスするには便利な方法です。
-// 
