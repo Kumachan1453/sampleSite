56eb52999993dab407cf56f28b39e59e786d688c
阪本周平 <github000only@gmail.com>
2021-09-11 13:26:18 +0900

    現時点で一通り作成した。この次からスライダーの実装に移る。

--- index.js
+++ index.js
@@ -3,9 +3,4 @@ document.getElementById("header").onclick = function(){
     document.querySelector('html').classList.toggle('open');
 }
 
-// document.getElementsByClassName("headerList").onclick = function(){
-    // console.log(`自動で戻る`);
-    // document.querySelector('headerList').classList.toggle('open');
-// }
-
 // Document の getElementById() メソッドは、 id プロパティが指定された文字列に一致する要素を表す Element オブジェクトを返します。要素の ID は指定されていれば固有であることが求められているため、特定の要素にすばやくアクセスするには便利な方法です。
